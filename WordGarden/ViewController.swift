63500995a053ef5af62c23ba86e7a9eeb998bb2b
rezakoushki95 <rezaaarhus@hotmail.com>
2022-09-01 19:38:47 +0200

    3.8  finish app: - add transition animation - use asyncAfter for delaying animation and sound

--- WordGarden/ViewController.swift
+++ WordGarden/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,8 @@ class ViewController: UIViewController {
 	@IBOutlet weak var gameStatusMessageLabel: UILabel!
 	@IBOutlet weak var playAgainButton: UIButton!
 	@IBOutlet weak var flowerImageView: UIImageView!
+	
+	var audioPlayer: AVAudioPlayer!
 	
 	var wordsToGuess = ["SWIFT", "DOG", "CAT"]
 	var currentWordIndex = 0
@@ -41,6 +44,19 @@ class ViewController: UIViewController {
 		wordBeingRevealedLabel.text = "_" + String(repeating: " _", count: wordToGuess.count-1)
 		updateGameStatusLabel()
 		
+	}
+	
+	func playSound(name: String) {
+		if let sound = NSDataAsset(name: name) {
+			do {
+				try audioPlayer = AVAudioPlayer(data: sound.data)
+				audioPlayer.play()
+			} catch {
+				print("Error: \(error.localizedDescription). Could not initialize AVAudioPlayer object")
+			}
+		} else {
+			print("ERROR: Could not read data from file \(name)")
+		}
 	}
 	
 	func returnLastCharacter(_ text: String) -> String {
@@ -97,17 +113,49 @@ class ViewController: UIViewController {
 		wordsInGameLabel.text = "Words in Game: \(wordsToGuess.count)"
 	}
 	
+	func drawFlowerAndPlaySound(_ currentLetterGuessed: String) {
+		// update image, if needed, and keep track of wrong guesses
+		if wordToGuess.contains(currentLetterGuessed) == false {
+			wrongGuessesRemaining -= 1
+			
+			DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+				UIView.transition(
+					with: self.flowerImageView,
+					duration: 0.5,
+					options: .transitionCrossDissolve,
+					animations: {self.flowerImageView.image = UIImage.init(named: "wilt\(self.wrongGuessesRemaining)")})
+				{ (_) in
+					
+					// If we are not on the last flower
+					// - show next flower
+					// otherwise we are on flower0
+					// - playSound (word-not-guessed)
+					// - perform another UIView Transition to Flower0
+					
+					if self.wrongGuessesRemaining != 0 {
+						self.flowerImageView.image = UIImage(named: "flower\(self.wrongGuessesRemaining)")
+					} else {
+						self.playSound(name: "word-not-guessed")
+						UIView.transition(
+							with: self.flowerImageView,
+							duration: 0.5,
+							options: .transitionCrossDissolve,
+							animations: {self.flowerImageView.image = UIImage(named: "flower\(self.wrongGuessesRemaining)")}, completion: nil)
+					}
+				}
+				self.playSound(name: "incorrect")
+			}
+		} else {
+			playSound(name: "correct")
+		}
+	}
+	
 	func guessALetter() {
 		// get current letter guessed and add it to all lettersGuessed
 		let currentLetterGuessed = guessedLetterTextField.text!
 		lettersGuessed += currentLetterGuessed
 		formatRevealedWord()
-		
-		// update image, if needed, and keep track of wrong guesses
-		if wordToGuess.contains(currentLetterGuessed) == false {
-			wrongGuessesRemaining -= 1
-			flowerImageView.image = UIImage(named: "flower\(wrongGuessesRemaining)")
-		}
+		drawFlowerAndPlaySound(currentLetterGuessed)
 		
 		// update gameStatusMessageLabel
 		guessCount += 1
@@ -125,6 +173,7 @@ class ViewController: UIViewController {
 		if wordBeingRevealedLabel.text!.contains("_") == false {
 			gameStatusMessageLabel.text = "You've guessed it! It took you \(guessCount) guesses to guess the word."
 			wordsGuessedCount += 1
+			playSound(name: "word-guessed")
 			updateAfterWinOrLose()
 		} else if wrongGuessesRemaining == 0 {
 			gameStatusMessageLabel.text = "So sorry. You are out of guesses."
@@ -139,7 +188,7 @@ class ViewController: UIViewController {
 	}
 	
 	@IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
-		sender.text = String(sender.text?.last ?? " ").trimmingCharacters(in: .whitespaces)
+		sender.text = String(sender.text?.last ?? " ").trimmingCharacters(in: .whitespaces).uppercased()
 		guessLetterButton.isEnabled = !(sender.text!.isEmpty)
 	}
 	
